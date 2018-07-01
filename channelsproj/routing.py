457f36778d50ec256a89aa93bd262468f0b88892
Arun Ravindran <arunvr@gmail.com>
2018-07-01 14:22:41 +0530

    Tick Tock Consumer works

--- channelsproj/routing.py
+++ channelsproj/routing.py
@@ -1,9 +1,9 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from notifier.consumers import EchoConsumer
+from notifier.consumers import TickTockConsumer
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
-        path("ws/", EchoConsumer),
+        path("ws/", TickTockConsumer),
     ])
 })
--- notifier/consumers.py
+++ notifier/consumers.py
@@ -1,15 +1,13 @@
-from channels.consumer import AsyncConsumer
+import asyncio
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class EchoConsumer(AsyncConsumer):
-    async def websocket_connect(self, event):
-        await self.send({
-            "type": "websocket.accept"
-        })
+class TickTockConsumer(AsyncJsonWebsocketConsumer):
 
-    async def websocket_receive(self, event):
-        # Echo the same received payload
-        await self.send({
-            "type": "websocket.send",
-            "text": event["text"]
-        })
+    async def connect(self):
+        await self.accept()
+        while 1:
+            await asyncio.sleep(1)
+            await self.send_json("tick")
+            await asyncio.sleep(1)
+            await self.send_json(".....tock")

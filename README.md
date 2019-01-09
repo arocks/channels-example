a2099f1fdc319a9410e424cb0a87b2901ff22ce5
Arun Ravindran <arunvr@gmail.com>
2019-01-09 13:18:59 +0530

    Updated to latest Django Channels and clarified README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Django Channels Examples
 
-Examples of asynchronous projects with Django Channels 2. Check https//arunrocks.com for the accompanying article and Youtube channel.
+Examples of asynchronous projects with Django Channels 2. Check out:
+
+* Detailed article: https://arunrocks.com/understanding-django-channels/
+* Explainer video: https://www.youtube.com/watch?v=G_EM5WM_08Q
 
 ## Setup
 
@@ -12,10 +15,27 @@ $ cd <proj-dir>
 $ pipenv install
 ```
 
+Start the redis server (and make sure it is running in the background)
+
+``` shellsession
+$ redis-server
+```
+
 Now you need to enter the pipenv shell to run the examples:
 
 ``` shellsession
 $ pipenv shell
 $ python manage.py migrate
+$ python manage.py createsuperuser
 $ python manage.py runserver
 ```
+
+You should see no notifications. Now open another browser window and log into Django admin. Add a new user. If you look
+at the first browser windows, then you should see the new user notification.
+
+### Other Examples
+
+Check out other branches of this git repository to see the other examples:
+
+* [EchoConsumer](https://github.com/arocks/channels-example/tree/EchoConsumer)
+* [TickTock](https://github.com/arocks/channels-example/tree/TickTock)
--- channelsproj/settings.py
+++ channelsproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hf178p075q1zi_!$o469jye^i+5sk=g4#*selbu6kg#zfesr42'
+SECRET_KEY = "hf178p075q1zi_!$o469jye^i+5sk=g4#*selbu6kg#zfesr42"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,46 +31,45 @@ ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    'channels',
-    'notifier',
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "channels",
+    "notifier",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'channelsproj.urls'
+ROOT_URLCONF = "channelsproj.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'channelsproj.wsgi.application'
+WSGI_APPLICATION = "channelsproj.wsgi.application"
 
 ASGI_APPLICATION = "channelsproj.routing.application"
 
@@ -78,9 +77,9 @@ ASGI_APPLICATION = "channelsproj.routing.application"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -90,26 +89,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -121,14 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Channels
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("localhost", 6379)],
-        },
-    },
+        "CONFIG": {"hosts": [("localhost", 6379)]},
+    }
 }
